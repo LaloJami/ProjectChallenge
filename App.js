1e841a1d7812b39652ddb18fba7fe8c3c3363e58
Eduardo Jami <eduardolalojami@gmail.com>
2021-11-09 13:41:27 -0500

    [add] Test of basic functions in react native

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ListStacK from './src/components/list/ListScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Open up App.js to start working on your app!</Text>
+      <Text style={styles.h2tittle}>Hello world!</Text>
+      <Text style={styles.h2tittle}>by Eduardo Jami</Text>
+      <ListStacK />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +18,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#292929',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    color: '#fff'
+  },
+  h2tittle: {
+    fontSize: 15,
+    textAlign: 'center',
+    color: '#fff'
+  }
 });
---
+++ src/components/list/ListScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, TextInput } from 'react-native';
+
+const ListScreen = () => {
+  return (
+    <View>
+        <Text>List Screen</Text>
+        <TextInput />
+        <Text>Prueba 1</Text>
+    </View>
+  );
+};
+
+export default ListScreen;
