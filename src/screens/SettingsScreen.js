45a671c5b20a41e0a3a29c198ba4510ee28fc6ec
Eduardo Jami <eduardolalojami@gmail.com>
2021-11-09 14:35:45 -0500

    [add] Stack navigator, add screens home, settings, list, and detail

---
+++ src/screens/SettingsScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function SettingsScreen() {
+  return (
+    <View>
+      <Text>Estamos en SettingsScreen</Text>
+      <Text>Estamos en SettingsScreen</Text>
+      <Text>Estamos en SettingsScreen</Text>
+      <Text>Estamos en SettingsScreen</Text>
+      <Text>Estamos en SettingsScreen</Text>
+    </View>
+  )
+}
