d8d9f35b8845bf8c756212509c4506df66738045
Eduardo Jami <eduardolalojami@gmail.com>
2021-11-10 09:07:15 -0500

    [update] adding styles to ListSiteCard

--- src/components/SiteCard.js
+++ src/components/SiteCard.js
@@ -10,13 +10,15 @@ export default function SiteCard(props) {
 
   return (
     <TouchableWithoutFeedback onPress={goToSite}>
-      <View>
-        <View>
-          <View>
-            {/* <Image source={{uri: site.image}}  /> */}
-            <View>
-              <Text>{site.name}</Text>
-              <Text>{site.address}</Text>
+      <View style={styles.card} >
+        <View style={styles.spacing} >
+          <View style={styles.bgStyles}>
+            <View style={styles.styleCardImage} >
+              <Image source={{uri: site.image}} style={styles.image} />
+            </View>
+            <View style={styles.styleCard}>
+              <Text style={styles.cardName}>{site.name}</Text>
+              <Text style={styles.cardAddress}>{site.address}</Text>
             </View>
           </View>
         </View>
@@ -25,3 +27,45 @@ export default function SiteCard(props) {
   )
 }
 
+const styles = StyleSheet.create({
+  card: {
+    flex: 1,
+    height: 130,
+  },
+  spacing: {
+    flex: 1,
+    padding: 5
+  },
+  bgStyles: {
+    backgroundColor: "grey",
+    flex: 1,
+    flexDirection: "row",
+    alignItems: 'center',
+    // justifyContent: 'center'
+  },
+  styleCardImage: {
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  styleCard: {
+    marginLeft: 10,
+    marginRight: 10,
+    flexDirection: 'column',
+    flex: 1
+  },
+  image: {
+    width: 90,
+    height: 90,
+  },
+  cardName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#fff'
+  },
+  cardAddress: {
+    fontSize: 18,
+    color: '#fff',
+    height: 'auto',
+  }
+
+})
