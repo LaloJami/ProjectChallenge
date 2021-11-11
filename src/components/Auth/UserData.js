3681357a1a3b356f651b786ec9c80be34a85464b
Eduardo Jami <eduardolalojami@gmail.com>
2021-11-11 09:31:29 -0500

    [add] include counter in User Account

--- src/components/Auth/UserData.js
+++ src/components/Auth/UserData.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {useState, useCallback} from 'react'
 import { StyleSheet, View, Text, Button } from 'react-native'
+import { useFocusEffect } from '@react-navigation/native';
+import { size } from 'lodash-es';
 import useAuth from '../../hooks/useAuth'
+import {getSiteFavoriteApi} from '../../api/favorite'
 
 export default function UserData() {
   const {auth, logout} = useAuth();
+  const [total, setTotal] = useState(0)
+
+  useFocusEffect(
+    useCallback(()=>{
+      (async () => {
+        try {
+          const response = await getSiteFavoriteApi();
+          setTotal(size(response))
+        } catch (error) {
+          setTotal(0)
+        }
+      })()
+    }, [])
+  )
 
   return (
     <View style={styles.content}>
@@ -15,7 +32,7 @@ export default function UserData() {
         <ItemMenu title="Name" text={`${auth.firstName} ${auth.lastName}`} />
         <ItemMenu title="Username" text={`${auth.username}`} />
         <ItemMenu title="Email" text={`${auth.email}`} />
-        <ItemMenu title="Total Favorite" text={`0 sites`} />
+        <ItemMenu title="Total Favorite" text={`${total} sites`} />
       </View>
       <Button title="Logout" onPress={logout} style={styles.btnLogoun} />
     </View>
